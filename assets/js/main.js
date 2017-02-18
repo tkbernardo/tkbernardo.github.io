4eb3f41ff7a26e96a0636d87d43fe104661e4722
Deepak Pradeep <dpac90@gmail.com>
2017-02-18 16:42:58 -0600

    Nav Header
    - Changed nav link icons to :before
    - Responsive fixes

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,7 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
-$('#nav-project-link').click(function() {
+$('#nav-projects-link').click(function() {
   $('#nav-projects').slideToggle("fast");
 });
 
